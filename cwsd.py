1c41f73485c65fe8b6049106958e74a677e48dd2
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2023-10-26 13:22:30 +0500

    Добавил метод add_fish для добавления новой рыбы в ПУСТОЙ бассейн.

--- cwsd.py
+++ cwsd.py
@@ -11,3 +11,20 @@ class CWSD:
 
         self.commercial_fish_mass: float = commercial_fish_mass
         self.package: int = package
+
+    def add_fish(self, new_fish: ListFish) -> bool:
+        """
+        Метод для добавления новой рыбы в ПУСТОЙ бассейн.
+        :param new_fish: Список новой рыбы.
+        :return: True, если был пустой бассейн и в него добавилась рыба, иначе - False.
+        """
+        # Найдем пустой бассейн
+        fish_were_added: bool = False
+
+        for pool in self.pools:
+            if pool.is_empty():
+                pool.add_fish(new_fish)
+                fish_were_added = True
+                break
+
+        return fish_were_added
